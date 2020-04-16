5197a2aa975d8b90b520062ce5b2bfb25c12c747
VictorLCosta <victorlc2019@outlook.com>
2020-04-16 13:04:49 -0300

    Project created

---
+++ OnlineSalesMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnlineSalesMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
