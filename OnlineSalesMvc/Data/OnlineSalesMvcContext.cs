84729a06b6ef192ba3cdc01ef07ca45c914a086c
VictorLCosta <victorlc2019@outlook.com>
2020-04-19 22:31:17 -0300

    CRUD Scaffolding

---
+++ OnlineSalesMvc/Data/OnlineSalesMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OnlineSalesMvc.Models;
+
+namespace OnlineSalesMvc.Data
+{
+    public class OnlineSalesMvcContext : DbContext
+    {
+        public OnlineSalesMvcContext (DbContextOptions<OnlineSalesMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<OnlineSalesMvc.Models.Department> Department { get; set; }
+    }
+}
--- OnlineSalesMvc/Startup.cs
+++ OnlineSalesMvc/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using OnlineSalesMvc.Data;
 
 namespace OnlineSalesMvc
 {
@@ -33,6 +35,9 @@ namespace OnlineSalesMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<OnlineSalesMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("OnlineSalesMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
