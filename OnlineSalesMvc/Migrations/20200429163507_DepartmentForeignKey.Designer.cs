87e8c6056950d9b4572c73e3b6d20b612972b894
VictorLCosta <victorlc2019@outlook.com>
2020-04-29 13:47:59 -0300

    Foreign key not null (referential integrity)

--- OnlineSalesMvc/Migrations/20200429163507_DepartmentForeignKey.Designer.cs
+++ OnlineSalesMvc/Migrations/20200429163507_DepartmentForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OnlineSalesMvc.Models;
 
 namespace OnlineSalesMvc.Migrations
 {
     [DbContext(typeof(OnlineSalesMvcContext))]
-    partial class OnlineSalesMvcContextModelSnapshot : ModelSnapshot
+    [Migration("20200429163507_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ OnlineSalesMvc/Migrations/20200429163507_DepartmentForeignKey.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineSalesMvc.Migrations
+{
+    public partial class DepartmentForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller");
+
+            migrationBuilder.DropColumn(
+                name: "Fk_id_Department",
+                table: "Seller");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Seller",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Seller",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Fk_id_Department",
+                table: "Seller",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
