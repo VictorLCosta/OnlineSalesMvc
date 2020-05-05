e5e8ed4c64a85bfe19a6ea65c8f24c50a7e1caaf
VictorLCosta <victorlc2019@outlook.com>
2020-05-04 22:33:06 -0300

    Implementing group search

--- OnlineSalesMvc/Models/SalesRecord.cs
+++ OnlineSalesMvc/Models/SalesRecord.cs
@@ -1,12 +1,17 @@
-﻿using OnlineSalesMvc.Models.Enums;
-using System;
+﻿using System;
+using OnlineSalesMvc.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace OnlineSalesMvc.Models
 {
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
         public Seller Seller { get; set; }
         public SaleStatus Status { get; set; }
